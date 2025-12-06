8399ec068e37c414c3d282563c00723620cbd864
AdebestAA <aadebesta@gmail.com>
2025-12-06 10:41:41 +0100

    changed details

--- src/app/utils/schoolAndDestinationsData.tsx
+++ src/app/utils/schoolAndDestinationsData.tsx
@@ -23,7 +23,21 @@ export const schoolsAndDestination:typeSchoolAndLocations[] = [
 {
     id:4,
   school:"FUOYE",
-  destinations:["Ibadan","Lagos","Osogbo"]
+  destinations:[
+    // Oye - ibada
+    "Oye-Ekiti - Ibadan(Premium Ride)",
+    "Oye-Ekiti - Ibadan(Regular Ride)",
+    // Ikole - ibadan
+    "Ikole - Ibadan(Premium Ride)",
+    "Ikole - Ibadan(Regular Ride)",
+// Oye to lagos
+    "Oye - Lagos(Premium Ride)",
+    "Oye - Lagos(Regular Ride)",
+// ikole to lagos 
+  "Ikole - Lagos(Premium Ride)",
+  "Ikole - Lagos(Regular Ride)",
+  ]
+  // destinations:["Ibadan(Premium Ride)","Ibadan(Regular Ride)","Lagos(Premium Ride)","Lagos(Regular Ride)","Osogbo"]
 },
 // {
 //     id:5,
@@ -50,30 +64,30 @@ export const schoolsAndDestination:typeSchoolAndLocations[] = [
   school:"PSSA AKURE",
   destinations:["Ibadan","OUI"],
 },
-{
-    id:10,
-  school:"Ibadan(Premium-Ride)",
-  destinations:["Ikole","Oye-Ekiti"],
-},
-{
-    id:11,
-  school:"Ibadan(Regular-Ride)",
-  destinations:["Ikole","Oye-Ekiti"],
-},
-{
-    id:12,
-  school:"Lagos(Premium-Ride)",
-  destinations:["Ikole","Oye"],
-},
-{
-    id:13,
-  school:"Lagos(Regular-Ride)",
-  destinations:["Ikole","Oye"],
-},
-{
-    id:14,
-  school:"Osun",
-  destinations:["Ikole","Oye"],
-},
+// {
+//     id:10,
+//   school:"Ibadan(Premium-Ride)",
+//   destinations:["Ikole","Oye-Ekiti"],
+// },
+// {
+//     id:11,
+//   school:"Ibadan(Regular-Ride)",
+//   destinations:["Ikole","Oye-Ekiti"],
+// },
+// {
+//     id:12,
+//   school:"Lagos(Premium-Ride)",
+//   destinations:["Ikole","Oye"],
+// },
+// {
+//     id:13,
+//   school:"Lagos(Regular-Ride)",
+//   destinations:["Ikole","Oye"],
+// },
+// {
+//     id:14,
+//   school:"Osun",
+//   destinations:["Ikole","Oye"],
+// },
 
 ]
